48a452e58053f026ea3ba99a74b5d8d8a19f8b0a
Victor Rosas <victor.rosas.96@hotmail.com>
2021-06-22 19:13:25 -0500

    fonts and colors

--- App.tsx
+++ App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
 import { SignIn } from './src/screens/SignIn';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <SignIn />
-  )
+  <>
+    <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+      <SignIn />
+  </>
+    )
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { 
   View, 
   Text,
-  Image,
-  StatusBar  
+  Image
 } from 'react-native';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -13,11 +12,6 @@ import { styles } from './styles';
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
 
       <Image 
         source={IllustrationImg} 
@@ -26,9 +20,16 @@ export function SignIn() {
       />
 
       <View style={styles.content}>
-        <Text style={styles.title}>Organize {`\n`} suas jogatinas {`\n`} facilmente</Text>
+        <Text style={styles.title}>
+          Conecte-se {'\n'} 
+          e organize suas {'\n'} 
+          jogatinas
+        </Text>
 
-        <Text style={styles.subtitle}>Crie grupos para jogar seus games {`\n`} favoritos com seus amigos</Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'} 
+          favoritos com seus amigos
+        </Text>
 
       <ButtonIcon 
         title="Entrar com o Discord"
