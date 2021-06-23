b744b68029fcca2b9100eeb67d0709438dccc724
Victor Rosas <victor.rosas.96@hotmail.com>
2021-06-22 19:26:12 -0500

    linear gradient

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 
 import { SignIn } from './src/screens/SignIn';
-import { StatusBar } from 'react-native';
+import { Background } from './src/components/Background';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,13 +21,13 @@ export default function App() {
   }
 
   return (
-  <>
+  <Background>
     <StatusBar 
       barStyle="light-content"
       backgroundColor="transparent"
       translucent
     />
       <SignIn />
-  </>
+  </Background>
     )
 }
